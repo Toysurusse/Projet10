a855659bd13462c8e4b387c7021c590466edf6c3
leb yep <max.leboiteux@gmail.com>
2018-11-20 20:04:02 +0100

    MAJ Dockerfile and maven docker plugin

--- spring-boot-soap/src/main/java/com/library/SpringBootLibraryApplication.java
+++ spring-boot-soap/src/main/java/com/library/SpringBootLibraryApplication.java
@@ -3,10 +3,11 @@ package com.library;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class SpringBootLibraryApplication implements CommandLineRunner {
+public class SpringBootLibraryApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLibraryApplication.class, args);
