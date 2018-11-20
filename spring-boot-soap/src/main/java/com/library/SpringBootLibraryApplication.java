31ac3cc2477211b746d4571d7eb111671719d525
leb yep <max.leboiteux@gmail.com>
2018-11-20 21:52:31 +0100

    MAJ WAR for Tomcat Deployment

--- spring-boot-soap/src/main/java/com/library/SpringBootLibraryApplication.java
+++ spring-boot-soap/src/main/java/com/library/SpringBootLibraryApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 @SpringBootApplication
@@ -24,4 +29,10 @@ public class SpringBootLibraryApplication extends SpringBootServletInitializer i
 
 	}
 
+	@GetMapping("/hello")
+	public Collection<String> sayHello() {
+		return IntStream.range(0, 10)
+				.mapToObj(i -> "Hello number " + i)
+				.collect(Collectors.toList());
+	}
 }
