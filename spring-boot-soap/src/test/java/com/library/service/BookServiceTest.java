4fb3956cf000c5a8eb7630f1dfd7f9213f863713
leb yep <max.leboiteux@gmail.com>
2018-11-27 11:52:46 +0100

    Test connectivity bdd

--- spring-boot-soap/src/test/java/com/library/service/BookServiceTest.java
+++ spring-boot-soap/src/test/java/com/library/service/BookServiceTest.java
@@ -24,7 +24,7 @@ public class BookServiceTest {
     public void testservicesBook () {
         List<Book> bookList = bookService.findAll();
         bookService.add(bookList.get(0));
-
+        System.out.println("Test BookName"+bookList.get(0).getBookName());
         bookService.findBySearch(bookList.get(0).getBookName());
         bookMapper.bookListEtoD(bookList);
     }
