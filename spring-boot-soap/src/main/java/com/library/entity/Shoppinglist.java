bf8fb0f2ee86ec7074284e841228a05c21537cbf
leb yep <max.leboiteux@gmail.com>
2018-11-30 16:06:09 +0100

    Maj autoincrement ID strategy

--- spring-boot-soap/src/main/java/com/library/entity/Shoppinglist.java
+++ spring-boot-soap/src/main/java/com/library/entity/Shoppinglist.java
@@ -6,13 +6,14 @@ import java.sql.Timestamp;
 
 @Entity
 @Table(name = "shoppinglist")
+@SequenceGenerator(name="seq", initialValue=1, allocationSize=100)
 public class Shoppinglist implements Serializable {
 
     private static final long serialVersionUID = 2L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", unique = true, nullable = false)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="seq")
+    @Column(name = "id")
     private int id;
 
     @Column(name = "idusershop")
